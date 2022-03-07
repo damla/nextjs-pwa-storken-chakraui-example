212e6c44ad4631885f5899bd28d0a075b2fd941e
Damla Koksal <me@damlakoksal.com>
2022-03-07 20:17:30 +0300

    feat: create component for Form

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,35 +2,27 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import {
   Box,
-  Button,
   Center,
   Container,
   Flex,
-  FormControl,
-  FormLabel,
   Heading,
   IconButton,
-  NumberDecrementStepper,
-  NumberIncrementStepper,
-  NumberInput,
-  NumberInputField,
-  NumberInputStepper,
   Spacer,
   StackDivider,
   useColorMode,
   useColorModeValue
 } from '@chakra-ui/react'
 import { MoonIcon, SunIcon } from '@chakra-ui/icons'
+import Form from '../components/form'
 
 const Home: NextPage = () => {
-  const { colorMode, toggleColorMode } = useColorMode()
+  const { toggleColorMode } = useColorMode()
 
   const bg = useColorModeValue('purple.100', 'gray.900')
   const bgContainer = useColorModeValue('purple.50', 'gray.700')
-  const inputBgColor = useColorModeValue('white', 'none')
   const color = useColorModeValue('purple.900', 'gray.100')
   const icon = useColorModeValue(<MoonIcon color='purple.900' />, <SunIcon />)
-  const buttonColorScheme = useColorModeValue('purple', 'blue')
+  const borderColor = useColorModeValue('purple.10', 'gray.200')
   return (
     <div>
       <Head>
@@ -41,8 +33,8 @@ const Home: NextPage = () => {
         />
       </Head>
 
-      <Box as='main' bg={bg} minHeight='100vh'>
-        <Flex as='header' minHeight='10vh'>
+      <Box as='main' bg={bg} h='100vh' maxH='100vh'>
+        <Flex as='header' h='10vh' maxH='10vh'>
           <Box p='10'>
             <Heading size='xl' as='h2' color={color}>
               Su Ne Kadar?
@@ -57,8 +49,8 @@ const Home: NextPage = () => {
             />
           </Box>
         </Flex>
-        <StackDivider borderColor='gray.200' />
-        <Center minHeight='90vh'>
+        <StackDivider borderColor={borderColor} />
+        <Center height='90vh' minH='90vh'>
           <Container
             bg={{ sm: 'none', md: bgContainer }}
             border={{ sm: '0', md: '1px' }}
@@ -67,44 +59,10 @@ const Home: NextPage = () => {
             p='4rem'
             centerContent
           >
-            <Heading size='lg' as='h3' color={color}>
+            <Heading size='xl' as='h1' color={color} pb='2rem'>
               💧
             </Heading>
-            <FormControl>
-              <FormLabel htmlFor='glass'>Cam Şişe</FormLabel>
-              <NumberInput size='md' defaultValue={1} max={50} mb='2rem'>
-                <NumberInputField
-                  id='glass'
-                  type='glass'
-                  bgColor={inputBgColor}
-                />
-                <NumberInputStepper>
-                  <NumberIncrementStepper />
-                  <NumberDecrementStepper />
-                </NumberInputStepper>
-              </NumberInput>
-              <FormLabel htmlFor='plastic'>Plastik Şişe</FormLabel>
-              <NumberInput size='md' defaultValue={1} max={50} mb='2rem'>
-                <NumberInputField
-                  id='plastic'
-                  type='plastic'
-                  bgColor={inputBgColor}
-                />
-                <NumberInputStepper>
-                  <NumberIncrementStepper />
-                  <NumberDecrementStepper />
-                </NumberInputStepper>
-              </NumberInput>
-              <Center>
-                <Button
-                  loadingText='Submitting'
-                  colorScheme={buttonColorScheme}
-                  variant='solid'
-                >
-                  ➕&nbsp;Hesapla
-                </Button>
-              </Center>
-            </FormControl>
+            <Form />
           </Container>
         </Center>
       </Box>
